6cfcf84c961070057afe612ede08f8840820aa8d
Kunal112003 <112599849+Kunal112003@users.noreply.github.com>
2023-08-02 07:34:55 -0600

    11th commit: working on expenses and add expenses

--- AIproject10/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/ai_project_10/databinding/FragmentBudgetBinding.java
+++ AIproject10/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/ai_project_10/databinding/FragmentBudgetBinding.java
@@ -4,10 +4,10 @@ package com.example.ai_project_10.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
 import androidx.viewbinding.ViewBindings;
 import com.example.ai_project_10.R;
@@ -17,19 +17,19 @@ import java.lang.String;
 
 public final class FragmentBudgetBinding implements ViewBinding {
   @NonNull
-  private final ConstraintLayout rootView;
+  private final FrameLayout rootView;
 
   @NonNull
-  public final TextView budget;
+  public final ImageView imageView;
 
-  private FragmentBudgetBinding(@NonNull ConstraintLayout rootView, @NonNull TextView budget) {
+  private FragmentBudgetBinding(@NonNull FrameLayout rootView, @NonNull ImageView imageView) {
     this.rootView = rootView;
-    this.budget = budget;
+    this.imageView = imageView;
   }
 
   @Override
   @NonNull
-  public ConstraintLayout getRoot() {
+  public FrameLayout getRoot() {
     return rootView;
   }
 
@@ -54,13 +54,13 @@ public final class FragmentBudgetBinding implements ViewBinding {
     // This is done to optimize the compiled bytecode for size and performance.
     int id;
     missingId: {
-      id = R.id.budget;
-      TextView budget = ViewBindings.findChildViewById(rootView, id);
-      if (budget == null) {
+      id = R.id.imageView;
+      ImageView imageView = ViewBindings.findChildViewById(rootView, id);
+      if (imageView == null) {
         break missingId;
       }
 
-      return new FragmentBudgetBinding((ConstraintLayout) rootView, budget);
+      return new FragmentBudgetBinding((FrameLayout) rootView, imageView);
     }
     String missingId = rootView.getResources().getResourceName(id);
     throw new NullPointerException("Missing required view with ID: ".concat(missingId));
